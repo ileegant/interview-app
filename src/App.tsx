91adadd157a84ba773d9a27a0b7e274bea572478
ileegant <osukhoruchko@gmail.com>
2026-02-11 13:32:28 +0200

    create basic App for showing question and toggle isLearned

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import "./App.css";
+import { useState } from "react";
+import type { IQuestion } from "./types";
+import { QuestionCard } from "./components/QuestionCard";
+
+const initialQuestion: IQuestion[] = [
+  {
+    id: 1,
+    question: "This is a question!",
+    answer: "This is an answer!",
+    difficulty: "easy",
+    isLearned: false,
+  },
+  {
+    id: 2,
+    question: "This is a second question!",
+    answer: "This is a second answer!",
+    difficulty: "easy",
+    isLearned: false,
+  },
+];
 
 function App() {
-  return <></>;
+  const [question, setQuestion] = useState<IQuestion[]>(initialQuestion);
+
+  const toggleStatus = (id: number) => {
+    setQuestion((prev) =>
+      prev.map((q) => (q.id === id ? { ...q, isLearned: !q.isLearned } : q))
+    );
+  };
+
+  return (
+    <div>
+      <h1>My Interview</h1>
+      {question.map((q) => (
+        <QuestionCard key={q.id} data={q} onToggle={toggleStatus} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
